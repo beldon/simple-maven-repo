8d13e5eb8c494d579763c4c9847020f218bbe5ec
Beldon <1047325205@qq.com>
2018-08-01 18:15:58 +0800

    init

---
+++ src/main/java/beldon/maven/MavenApplication.java
@@ -0,0 +1,12 @@
+package beldon.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+}
---
+++ src/test/java/beldon/maven/MavenApplicationTests.java
@@ -0,0 +1,16 @@
+package beldon.maven;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
